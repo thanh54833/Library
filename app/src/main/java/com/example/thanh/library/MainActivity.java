379552ec7b5bec37b736a7324eafbfaadc23e662
the gioi cua thanh <pham54833@gmail.com>
2018-09-22 13:09:56 +0700

    1)create library file ... +++ create funtion read file  ... +++ create funtion write file v2 ... +++ create funtion delete file ... +++ create funtion coppy file ...

--- app/src/main/java/com/example/thanh/library/MainActivity.java
+++ app/src/main/java/com/example/thanh/library/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.thanh.library;
 import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,19 +23,33 @@ public class MainActivity extends AppCompatActivity {
         textView=findViewById(R.id.main);
 
         File extStore = Environment.getExternalStorageDirectory();
-        File fileread=new java.io.File(extStore+"/test.txt");
-        File filewrite=new java.io.File(extStore+"/thanh/thanh.txt");
+        File fileread=new File(extStore+"/test.txt");
+        File filewrite=new File(extStore+"/thanh/thanh.txt");
+
+        File filecopy=new File(extStore+"/test/thanh123");
+        File filecopy1=new File(extStore+"/test1");
+
+        File[] filezip=new File[3];
+
+        filezip[0]=new File(extStore+"/text1.doc");
+        filezip[1]=new File(extStore+"/hello.txt");
+        filezip[2]=new File(extStore+"/test");
 
         // read file
         //textView.append(text);
         //textView.setText(null);
-
-        //Toast.makeText(this,"result :"+Files.wirteFile(filewrite,Files.readFile(fileread)),Toast.LENGTH_SHORT).show();
+        //Toast.makeText(this,"result :"+Files.wirteFile(null,Files.readFile(null)),Toast.LENGTH_SHORT).show();
         //if(Files.readFile(fileread)!=null) {
         //    textView.append(new String(Files.readFile(fileread)));
         //}
+        //Toast.makeText(getApplicationContext(),"result :"+Files.delete(fileread),Toast.LENGTH_SHORT).show();
+        //Toast.makeText(getApplicationContext(),"result :"+Files.copy(filecopy,filecopy1),Toast.LENGTH_SHORT).show();
 
-        Toast.makeText(getApplicationContext(),"result :"+Files.delete(fileread),Toast.LENGTH_SHORT).show();
+        //Toast.makeText(getApplicationContext(),"result :"+Files.zip(filezip,new File(extStore+"/zip")),Toast.LENGTH_SHORT).show();
+
+        Toast.makeText(getApplicationContext(),"result :"+Files.zipDir(new File(extStore+"/test"),new File(extStore+"/zip")),Toast.LENGTH_SHORT).show();
+
+
 
 
 
--- mylibrary/src/main/java/com/example/mylibrary/Files.java
+++ mylibrary/src/main/java/com/example/mylibrary/Files.java
@@ -1,5 +1,7 @@
 package com.example.mylibrary;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -7,12 +9,15 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class Files {
 
-    public static byte[] readFile(File path)  {
-        if(!path.exists()||path==null){
-            if(BuildConfig.DEBUG){
+    public static byte[] readFile(File sourceLocation) {
+        if (sourceLocation == null || !sourceLocation.exists()) {
+            if (BuildConfig.DEBUG) {
                 Util.messageDisplay("Read file error : file no exists");
             }
             return null;
@@ -22,91 +27,245 @@ public class Files {
         try {
             byte[] buffer = new byte[4096];
             ous = new ByteArrayOutputStream();
-            ios = new FileInputStream(String.valueOf(path));
+            ios = new FileInputStream(String.valueOf(sourceLocation));
             int read = 0;
             while ((read = ios.read(buffer)) != -1) {
                 ous.write(buffer, 0, read);
             }
             return ous.toByteArray();
         } catch (FileNotFoundException e) {
-            if(BuildConfig.DEBUG){
-                Util.messageDisplay("Read file error :"+e.getMessage());
+            if (BuildConfig.DEBUG) {
+                Util.messageDisplay("Read file error :" + e.getMessage());
             }
             return null;
 
         } catch (IOException e) {
 
-            if(BuildConfig.DEBUG){
-                Util.messageDisplay("Read file error :"+e.getMessage());
+            if (BuildConfig.DEBUG) {
+                Util.messageDisplay("Read file error :" + e.getMessage());
             }
             return null;
-        }
-        finally {
+        } finally {
             try {
                 if (ous != null)
                     ous.close();
                 if (ios != null)
                     ios.close();
-            }
-            catch (IOException e) {
-                if(BuildConfig.DEBUG){
-                    Util.messageDisplay("Read file error :"+e.getMessage());
+            } catch (IOException e) {
+                if (BuildConfig.DEBUG) {
+                    Util.messageDisplay("Read file error :" + e.getMessage());
                 }
                 return null;
             }
         }
     }
 
-    public static boolean wirteFile(File path, byte[] buffer){
-        File parent=new File(path.getParent());
-        if(!parent.exists())
-        {
-            parent.mkdirs();
-            if(BuildConfig.DEBUG){
-                Util.messageDisplay("Write file error : Create parent file "+parent);
-            }
-        }
-        if(buffer==null||path==null){
-            if(BuildConfig.DEBUG){
+    public static boolean wirteFile(File sourceLocation, byte[] buffer) {
+        if (buffer == null || sourceLocation == null) {
+            if (BuildConfig.DEBUG) {
                 Util.messageDisplay("Write file error : message null");
             }
             return false;
         }
-        FileOutputStream fos=null;
+
+        File parent = new File(sourceLocation.getParent());
+        if (!parent.exists()) {
+            parent.mkdirs();
+            if (BuildConfig.DEBUG) {
+                Util.messageDisplay("Write file error : Create parent file " + parent);
+            }
+        }
+
+        FileOutputStream fos = null;
         try {
-            fos = new FileOutputStream(String.valueOf(path));
+            fos = new FileOutputStream(String.valueOf(sourceLocation));
             fos.write(buffer);
             fos.close();
             return true;
         } catch (IOException e) {
-            if(BuildConfig.DEBUG){
-                Util.messageDisplay("Write file error :"+e.getMessage());
+            if (BuildConfig.DEBUG) {
+                Util.messageDisplay("Write file error :" + e.getMessage());
             }
             return false;
         }
     }
 
-    public static boolean delete(File path){
-        if(!path.exists()||path==null){
-            if(BuildConfig.DEBUG){
+    public static boolean delete(File sourceLocation) {
+        if (sourceLocation == null || !sourceLocation.exists()) {
+            if (BuildConfig.DEBUG) {
                 Util.messageDisplay("delete file error : path no exists ");
             }
             return false;
         }
-        if (path.isDirectory()) {
-            for (File f : path.listFiles()) {
+        if (sourceLocation.isDirectory()) {
+            for (File f : sourceLocation.listFiles()) {
                 delete(f);
             }
             return true;
         } else {
-            path.delete();
+            sourceLocation.delete();
             return true;
         }
     }
 
 
+    public static boolean copy(File sourceLocation, File targetLocation) {
+        if(sourceLocation==null||targetLocation==null||!sourceLocation.exists()){
+            if (BuildConfig.DEBUG) {
+                Util.messageDisplay("Coppy file error : file no exists");
+            }
+            return false;
+        }
+        if (sourceLocation.isDirectory()) {
+            if (!targetLocation.exists()) {
+                targetLocation.mkdirs();
+            }
+            String[] children = sourceLocation.list();
+            for (int i = 0; i < children.length; i++) {
+                copy(new File(sourceLocation, children[i]), new File(targetLocation, children[i]));
+            }
+            return true;
+        } else {
+            return copyFile(sourceLocation, targetLocation);
+        }
+    }
+
+    public static boolean copyFile(File sourceLocation, File targetLocation) {
+
+        InputStream in = null;
+        OutputStream out=null;
+        try {
+            in = new FileInputStream(sourceLocation);
+            out = new FileOutputStream(targetLocation);
+
+            byte[] buf = new byte[1024];
+            int len;
+            while ((len = in.read(buf)) > 0) {
+                out.write(buf, 0, len);
+            }
+            in.close();
+            out.close();
+
+            return true;
+        } catch (FileNotFoundException e) {
+            if (BuildConfig.DEBUG) {
+                Util.messageDisplay("Coppy file error :"+e.getMessage());
+            }
+            return false;
+        } catch (IOException e) {
+            if (BuildConfig.DEBUG) {
+                Util.messageDisplay("Coppy file error :"+e.getMessage());
+            }
+            return false;
+        }
+    }
+
+    public static boolean zip(File[] sourceLocation, File targetLocation) {
+
+        if(sourceLocation==null||targetLocation==null){
+            if (BuildConfig.DEBUG) {
+                Util.messageDisplay("Zip file error : file no exists");
+            }
+            return false;
+        }
+        try {
+            BufferedInputStream origin = null;
+            FileOutputStream dest = new FileOutputStream(targetLocation);
+            ZipOutputStream out = new ZipOutputStream(new BufferedOutputStream(dest));
+            byte data[] = new byte[1024];
+
+            for (int i = 0; i < sourceLocation.length; i++) {
+
+                if(sourceLocation[i]==null||!sourceLocation[i].exists()){
+                    if (BuildConfig.DEBUG) {
+                        Util.messageDisplay("Zip file error : file no exists");
+                    }
+                    return false;
+                }
+                if(sourceLocation[i].isFile())
+                {
+                    FileInputStream fi = new FileInputStream(sourceLocation[i]);
+                    origin = new BufferedInputStream(fi, 1024);
+                    ZipEntry entry = new ZipEntry(sourceLocation[i].getName());
+
+                    out.putNextEntry(entry);
+                    int count;
+                    while ((count = origin.read(data, 0, 1024)) != -1) {
+                        out.write(data, 0, count);
+                    }
+
+                }
+
+                origin.close();
+
+            }
+            out.close();
+            return true;
+
+        } catch (Exception e) {
+            if (BuildConfig.DEBUG) {
+                Util.messageDisplay("Zip file error :"+e.getMessage());
+            }
+            return false;
+        }
+    }
 
 
+
+    public static boolean zipDir(File zipFileName, File dir){
+
+        File dirObj = new File(String.valueOf(dir));
+        ZipOutputStream out = null;
+        try {
+
+            out = new ZipOutputStream(new FileOutputStream(zipFileName));
+            System.out.println("Creating : " + zipFileName);
+            addDir(dirObj, out);
+            out.close();
+            return true;
+
+        } catch (FileNotFoundException e) {
+            if (BuildConfig.DEBUG) {
+                Util.messageDisplay("Zip file error :"+e.getMessage());
+            }
+            return false;
+        } catch (IOException e) {
+            if (BuildConfig.DEBUG) {
+                Util.messageDisplay("Zip file error :"+e.getMessage());
+            }
+            return false;
+        }
+    }
+
+    static void addDir(File dirObj, ZipOutputStream out)  {
+        File[] files = dirObj.listFiles();
+        byte[] tmpBuf = new byte[1024];
+
+        for (int i = 0; i < files.length; i++) {
+            if (files[i].isDirectory()) {
+                addDir(files[i], out);
+                continue;
+            }
+            FileInputStream in = null;
+            try {
+                in = new FileInputStream(files[i].getAbsolutePath());
+                System.out.println(" Adding: " + files[i].getAbsolutePath());
+                out.putNextEntry(new ZipEntry(files[i].getAbsolutePath()));
+                int len;
+                while ((len = in.read(tmpBuf)) > 0) {
+                    out.write(tmpBuf, 0, len);
+                }
+                out.closeEntry();
+                in.close();
+
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
 
 
 
