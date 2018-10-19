5273c8ae78ec1fc8bb9196143867e18f17100a9d
the gioi cua thanh <pham54833@gmail.com>
2018-10-19 15:58:08 +0700

    1)create library file ... +++ create funtion read file  ... +++ create funtion write file v2 ... +++ create funtion delete file ... +++ create funtion coppy file ... +++ create funtion zip file... +++ finish library file ... +++ create funtion cipher md5, sha1, des, aes, not, xor ,shift... +++ create funtion downlaod file .. +++ create funtion backup and recovery.. +++ create funtin toast ... +++ create funtion downlaod by url connection... +++ finish library backup and recovery... 2)App cast Android to pc laptop screen... +++ create module laptop screen... +++ create funtion httpServerSocket ... 3) App movie +++ demo MVP +++ demo retrofix +++ finish code login (MVP,retrofit) +++ push github

--- myapplication/src/main/java/com/example/myapplication/view/ILoginListenerView.java
+++
@@ -1,8 +0,0 @@
-package com.example.myapplication.view;
-
-import com.example.myapplication.retrofix.model.Logins;
-
-public interface ILoginListenerView {
-    void getDataSuccess(Logins logins);
-    void getMessageError(String e);
-}
